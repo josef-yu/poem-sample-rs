3d157dc14c51125140396911d9a858daf55b7199
Joseph Yu <slay3r28@gmail.com>
2025-02-12 02:01:30 +0800

    Update auth routes, models and tests to poem open api

--- src/auth/model.rs
+++ src/auth/model.rs
@@ -1,6 +1,9 @@
 use poem::{http::StatusCode, Error, FromRequest, Result};
+use poem_openapi::{payload::Json, Object};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+
+use crate::response::{CreateResponse, Detail};
 
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -22,7 +25,7 @@ impl User {
     }
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Object)]
 pub struct UserFormBody {
     pub username: String,
     pub password: String
@@ -44,13 +47,35 @@ impl<'a> FromRequest<'a> for UserFormBody {
     }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Object)]
 pub struct LoginResponse {
     pub token: String
+}
+
+impl LoginResponse {
+    pub fn new(token: String) -> CreateResponse<LoginResponse> {
+        let body = LoginResponse{ token };
+
+        CreateResponse::Ok(Json(body))
+    }
 }
 
 impl From<LoginResponse> for Value {
     fn from(value: LoginResponse) -> Self {
         serde_json::to_value(value).unwrap()
+    }
+}
+
+pub trait RegisterReponse {
+    fn success() -> Self;
+}
+
+impl RegisterReponse for CreateResponse<Detail> {
+    fn success() -> Self {
+        let detail = Detail {
+            message: "User registered successfully".to_string()
+        };
+
+        Self::Created(Json(detail))
     }
 }
--- src/auth/route.rs
+++ src/auth/route.rs
@@ -1,95 +1,90 @@
 use std::sync::{Arc, Mutex};
 
-use poem::{handler, http::StatusCode, post, web::Data, Error, Result, Route};
-use serde_json::Value;
+use poem::{web::Data, Result};
+use poem_openapi::{payload::Json, OpenApi};
 
-use crate::{auth::model::{UserFormBody, LoginResponse, User}, db::Db, response::GenericResponse};
+use crate::{auth::model::{LoginResponse, User, UserFormBody}, db::Db, response::{CreateResponse, Detail, GenericError}};
 
-use super::jwt;
+use super::{jwt, model::RegisterReponse};
 
 pub const USER_TABLE_NAME: &str = "user";
 
-#[handler]
-pub fn login(payload: UserFormBody, db: Data<&Arc<Mutex<Db>>>, manager: Data<&jwt::Manager>) -> Result<GenericResponse<LoginResponse>> {
-    let db_ref = db
-        .lock()
-        .map_err(|_| Error::from_status(StatusCode::INTERNAL_SERVER_ERROR))
-        .expect("Getting db lock");
-    let user = db_ref.find_by_value::<User>(USER_TABLE_NAME.to_string(), "username".to_string(), payload.username)
-        .map(|x| x.first().cloned().unwrap())
-        .ok_or(
-            Error::from_status(StatusCode::UNAUTHORIZED)
-        )?;
-    
-    if user.password != payload.password {
-        return Err(Error::from_status(StatusCode::UNAUTHORIZED))
+pub struct AuthApi;
+
+#[poem_grants::open_api]
+#[OpenApi]
+impl AuthApi {
+
+    #[oai(path = "/login", method = "post")]
+    pub async fn login(&self, payload: UserFormBody, db: Data<&Arc<Mutex<Db>>>, manager: Data<&jwt::Manager>) -> Result<CreateResponse<LoginResponse>> {
+        let db_ref = db
+            .lock()
+            .map_err(|_| GenericError::DbLock)?;
+
+        let user = db_ref.find_by_value::<User>(USER_TABLE_NAME.to_string(), "username".to_string(), payload.username)
+            .map(|x| x.first().cloned().unwrap())
+            .ok_or(GenericError::not_authorized())?;
+        
+        if user.password != payload.password {
+            return Err(GenericError::not_authorized().into())
+        }
+
+        let token_data = manager.create_token_data(user.username, user.permissions);
+        let token = manager.encode(token_data)
+            .map_err(|_| GenericError::JwtEncoding)?;
+
+        Ok(LoginResponse::new(token))
     }
 
-    let token_data = manager.create_token_data(user.username, user.permissions);
-    let token = manager.encode(token_data)
-        .expect("Encoding jwt");
+    #[oai(path = "/register", method = "post")]
+    pub async fn register(&self, payload: Json<UserFormBody>, db: Data<&Arc<Mutex<Db>>>) -> Result<CreateResponse<Detail>> {
+        let mut db_ref = db
+            .lock()
+            .map_err(|_| GenericError::DbLock)?;
 
-    Ok(GenericResponse{
-        status_code_u16: StatusCode::OK.as_u16(),
-        message: None,
-        data: Some(LoginResponse{ token })
-    })
-}
+        let users = db_ref
+            .find_by_value::<User>(USER_TABLE_NAME.to_string(), "username".to_string(), payload.0.username.clone())
+            .ok_or(GenericError::DbOperation)?;
+        
+        if !users.is_empty() {
+            let detail = Detail {
+                message: "User already exists!".to_string()
+            };
 
-#[handler]
-pub fn register(payload: UserFormBody, db: Data<&Arc<Mutex<Db>>>) -> Result<GenericResponse<Value>> {
-    let mut db_ref = db
-        .lock()
-        .map_err(|_| Error::from_status(StatusCode::INTERNAL_SERVER_ERROR))
-        .expect("Getting db lock");
-    let users = db_ref
-        .find_by_value::<User>(USER_TABLE_NAME.to_string(), "username".to_string(), payload.username.clone())
-        .ok_or(
-            Error::from_status(StatusCode::UNAUTHORIZED)
-        )?;
-    
-    if !users.is_empty() {
-        return Ok(GenericResponse{
-            status_code_u16: StatusCode::BAD_REQUEST.as_u16(),
-            message: Some("User already exists!".to_string()),
-            data: None
-        })
+            return Err(GenericError::BadRequest(Json(detail)).into())
+        }
+        
+        let id = db_ref
+            .get_increment_last_id(USER_TABLE_NAME.to_string())
+            .map_err(|_| GenericError::DbOperation)?
+            .ok_or(GenericError::TableNotFound)?;
+        
+        // Skipping hashing of password
+        let to_insert = User::new(id, payload.0.username, payload.0.password, vec!["MUTATE".to_string()]);
+        
+        let inserted_user = db_ref
+            .insert_or_update(USER_TABLE_NAME.to_string(), id, to_insert)
+            .map_err(|_| GenericError::DbOperation)?;
+
+        if inserted_user.is_none() {
+            return Err(GenericError::DbOperation.into())
+        }
+
+        Ok(CreateResponse::success())
     }
-    let id = db_ref.get_increment_last_id(USER_TABLE_NAME.to_string()).unwrap().unwrap();
-    // Skipping hashing of password
-    let to_insert = User::new(id, payload.username, payload.password, vec!["MUTATE".to_string()]);
-    db_ref
-        .insert_or_update(USER_TABLE_NAME.to_string(), id, to_insert)
-        .unwrap()
-        .unwrap();
-
-    Ok(GenericResponse::<Value>{
-        message: Some("User registered successfully.".to_string()),
-        status_code_u16: StatusCode::CREATED.as_u16(),
-        data: None
-    })
-}
-
-pub fn auth_routes() -> Route {
-    Route::new()
-        .at("/login", post(login))
-        .at("/register", post(register))
 }
 
 
 #[cfg(test)]
 mod tests {
-    use poem::Endpoint;
+    use poem::{http::StatusCode, Endpoint};
 
-    use crate::test::{async_run_with_file_create_teardown, ApiTestClient, TEST_PASSWORD, TEST_USERNAME};
+    use crate::test::{async_run_with_file_create_teardown, OpenApiTestClient, TEST_PASSWORD, TEST_USERNAME};
 
     use super::*;
 
-    fn init_client(file_name: String) -> ApiTestClient<impl Endpoint> {
-        let routes = Route::new().nest(
-            "/", auth_routes()
-        );
-        let test_client = ApiTestClient::init(routes, file_name.as_str());
+    fn init_api_client(file_name: String) -> OpenApiTestClient<impl Endpoint> {
+        let test_client = OpenApiTestClient::init(AuthApi, file_name.as_str());
         {
             let mut db = test_client.db.lock().unwrap();
             db.add_table(USER_TABLE_NAME.to_string(), false).unwrap();
@@ -114,11 +109,10 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_login() {
+    async fn test_api_login() {
         async_run_with_file_create_teardown(|file_name| {
-            let file_name = file_name.to_string();
             async {
-                let test_client = init_client(file_name);
+                let test_client = init_api_client(file_name);
                 {
                     let mut db = test_client.db.lock().unwrap();
                     insert_user(&mut db, TEST_USERNAME, TEST_PASSWORD);
@@ -138,11 +132,10 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_register() {
+    async fn test_api_register() {
         async_run_with_file_create_teardown(|file_name| {
-            let file_name = file_name.to_string();
             async {
-                let test_client = init_client(file_name);
+                let test_client = init_api_client(file_name);
 
                 let response = test_client.client.post("/register")
                     .body_json(&UserFormBody{ 
