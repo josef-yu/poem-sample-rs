e66412adc6de4e0f8f827633a5164ec5b8fbcd24
Joseph Yu <slay3r28@gmail.com>
2025-02-12 02:57:18 +0800

    Add clap arguments and env for config

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ pub mod auth;
 use std::sync::{Arc, Mutex};
 
 use auth::route::AuthApi;
+use clap::Parser;
 use items::route::ItemsApi;
 use poem::middleware::{AddData, Tracing};
 use poem::Middleware;
@@ -15,8 +16,28 @@ use poem_openapi::OpenApiService;
 
 use crate::db::Db;
 
+#[derive(Debug, Parser)]
+#[clap(author, version, about, disable_help_flag = true)]
+pub struct ServerConfig {
+    #[clap(short, long, env = "HOST", default_value = "0.0.0.0")]
+    /// The binding host address of the server.
+    pub host: String,
+
+    #[clap(short, long, env = "PORT", default_value = "3000")]
+    pub port: u16,
+
+    #[clap(short = 's', long, env = "JWT_SECRET", default_value = "secret")]
+    pub jwt_secret: String,
+
+    #[clap(short = 'd', long, env = "JWT_HOUR_DURATION", default_value = "24")]
+    pub jwt_hour_duration: i64,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let config = ServerConfig::parse();
+    let address = format!("{}:{}", config.host, config.port);
+
     tracing_subscriber::fmt()
         .with_env_filter("poem=trace")
         .init();
@@ -28,7 +49,7 @@ async fn main() -> Result<(), std::io::Error> {
 
     let api_service = OpenApiService::new((AuthApi, ItemsApi), "Poem api", "1");
 
-    let jwt_manager = auth::jwt::Manager::init("secret".to_string(), 24);
+    let jwt_manager = auth::jwt::Manager::init(config.jwt_secret, config.jwt_hour_duration);
     let jwt_middleware = auth::middleware::JwtMiddleware{ manager: jwt_manager.clone() };
     
     let app = Route::new()
@@ -39,7 +60,7 @@ async fn main() -> Result<(), std::io::Error> {
                 .combine(AddData::new(jwt_manager))
                 .combine(Tracing)
         );
-    Server::new(TcpListener::bind("0.0.0.0:3000"))
+    Server::new(TcpListener::bind(address))
         .run(app)
         .await
 }
