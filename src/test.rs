35493ccec150d9a174364aac0f59d445bc0abd58
Joseph Yu <slay3r28@gmail.com>
2025-02-12 02:00:25 +0800

    Update test utils with poem open api client

--- src/test.rs
+++ src/test.rs
@@ -6,7 +6,8 @@ use std::sync::{Arc, Mutex};
 use futures::FutureExt;
 use poem::middleware::{AddData, Middleware};
 use poem::test::TestClient;
-use poem::{Endpoint, EndpointExt, IntoEndpoint};
+use poem::{Endpoint, EndpointExt, IntoEndpoint, Route};
+use poem_openapi::OpenApiService;
 use serde_json::Value;
 use uuid::Uuid;
 
@@ -22,53 +23,51 @@ pub const TEST_PERMISSION: &str = "MUTATE";
 
 
 pub fn run_with_file_create_teardown<T>(test: T)
-    where T: FnOnce(&str) + panic::UnwindSafe
+    where T: FnOnce(String) + panic::UnwindSafe
 {
     let uuid = Uuid::new_v4().to_string();
     let file_name = format!("./{}-{}", uuid, TEST_FILE_NAME);
-    let file_name_str = file_name.as_str();
 
     let _ = File::create(file_name.clone());
 
     let result = panic::catch_unwind(|| {
-        test(file_name_str)
+        test(file_name.clone())
     });
 
-    let _ = std::fs::remove_file(file_name);
+    let _ = std::fs::remove_file(file_name.clone());
 
     assert!(result.is_ok())
 }
 
 
 pub async fn async_run_with_file_create_teardown<T, U>(test: T)
-    where T: FnOnce(&str) -> U + panic::UnwindSafe,
+    where T: FnOnce(String) -> U + panic::UnwindSafe,
         U: Future<Output = ()>
 {
     let uuid = Uuid::new_v4().to_string();
     let file_name = format!("./{}-{}", uuid, TEST_FILE_NAME);
-    let file_name_str = file_name.as_str();
 
     let _ = File::create(file_name.clone());
 
-    let result = AssertUnwindSafe(test(file_name_str))
+    let result = AssertUnwindSafe(test(file_name.clone()))
         .catch_unwind()
         .await;
 
-    let _ = std::fs::remove_file(file_name);
+    let _ = std::fs::remove_file(file_name.clone());
 
     assert!(result.is_ok())
 }
 
-pub struct ApiTestClient<E> {
+pub struct OpenApiTestClient<E> {
     pub db: Arc<Mutex<Db>>,
     pub client: TestClient<E>,
     pub jwt_manager: auth::jwt::Manager,
     pub token: String
 }
 
-impl<E: Endpoint + EndpointExt> ApiTestClient<E> {
-    pub fn init<T>(route: T, file_name: &str) -> ApiTestClient<impl Endpoint + EndpointExt> 
-        where T: IntoEndpoint<Endpoint = E>
+impl<E: Endpoint + EndpointExt + 'static > OpenApiTestClient<E> {
+    pub fn init<T>(api: T, file_name: &str) -> OpenApiTestClient<impl Endpoint + EndpointExt> 
+        where OpenApiService<T, ()>: IntoEndpoint<Endpoint = E>
     {
         let db = Db::init(file_name.to_string()).unwrap();
         let arc_db = Arc::new(Mutex::new(db));
@@ -78,8 +77,11 @@ impl<E: Endpoint + EndpointExt> ApiTestClient<E> {
         let jwt_data = jwt_manager.create_token_data(TEST_USERNAME.to_string(), vec![TEST_PERMISSION.to_string()]);
         let token = jwt_manager.encode(jwt_data).unwrap();
 
+        let api_service = OpenApiService::new(api, "test api", "1");
+
         let client = TestClient::new(
-        route
+        Route::new()
+            .nest("", api_service)
             .with(
     jwt_middleware
                     .combine(AddData::new(arc_db.clone()))
@@ -94,7 +96,7 @@ impl<E: Endpoint + EndpointExt> ApiTestClient<E> {
             })
         );
 
-        ApiTestClient {
+        OpenApiTestClient {
             db: arc_db,
             jwt_manager,
             client,
