f241cda740047c8fefb61420387f81be9311d8ca
Joseph Yu <slay3r28@gmail.com>
2025-02-12 02:01:49 +0800

    Use poem open api on server start

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,13 @@ pub mod auth;
 
 use std::sync::{Arc, Mutex};
 
-use auth::route::auth_routes;
+use auth::route::AuthApi;
+use items::route::ItemsApi;
 use poem::middleware::{AddData, Tracing};
 use poem::Middleware;
 use poem::{listener::TcpListener, EndpointExt, Route, Server};
-use response::GenericResponse;
-use serde_json::Value;
+use poem_openapi::OpenApiService;
 
-use crate::items::route::item_routes;
 use crate::db::Db;
 
 #[tokio::main]
@@ -27,25 +26,19 @@ async fn main() -> Result<(), std::io::Error> {
     db.add_table("user".to_string(), false).unwrap();
     let db_ref = Arc::new(Mutex::new(db));
 
+    let api_service = OpenApiService::new((AuthApi, ItemsApi), "Poem api", "1");
+
     let jwt_manager = auth::jwt::Manager::init("secret".to_string(), 24);
     let jwt_middleware = auth::middleware::JwtMiddleware{ manager: jwt_manager.clone() };
     
     let app = Route::new()
-        .nest("/items", item_routes())
-        .nest("/", auth_routes())
+        .nest("", api_service)
         .with(
             jwt_middleware
                 .combine(AddData::new(db_ref))
                 .combine(AddData::new(jwt_manager))
                 .combine(Tracing)
-        )
-        .catch_all_error(|err| async move {
-            GenericResponse::<Value>{ 
-                message: Some(err.to_string()),
-                status_code_u16: err.status().as_u16(),
-                data: None
-            }
-        });
+        );
     Server::new(TcpListener::bind("0.0.0.0:3000"))
         .run(app)
         .await
